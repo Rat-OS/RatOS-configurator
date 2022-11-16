623e9f267268092e29e50c1055c2c0a1c1fcdb90
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2022-11-16 22:52:42 +0100

    Scripts: fix message in board-script

--- src/scripts/board-script.sh
+++ src/scripts/board-script.sh
@@ -3,6 +3,6 @@ if [ ! "$EUID" -eq 0 ]; then
 	echo "This script must run as root"
 	exit -1
 fi
-echo "Compiling firmware at /home/pi/klipper_config/config/boards/$1..."
+echo "Running board script /home/pi/klipper_config/config/boards/$1\n\n"
 /home/pi/klipper_config/config/boards/$1
 chown -R pi:pi /home/pi/klipper/
