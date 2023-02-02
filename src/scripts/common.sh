81642b5df663fd710ed2e73d00a397088bac56f3
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-02-02 02:12:29 +0100

    Scripts: run setup script as root

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -16,17 +16,17 @@ report_status()
 
 pnpm_install() {
     pushd $SCRIPT_DIR/..
-    sudo pnpm install
+    pnpm install
     popd
 }
 
 ensure_pnpm_installation() {
-	if ! sudo which pnpm &> /dev/null; then
+	if ! which pnpm &> /dev/null; then
 		report_status "Installing pnpm"
-		sudo curl -fsSL https://get.pnpm.io/install.sh | sudo sh -
-		sudo source /root/.bashrc
+		curl -fsSL https://get.pnpm.io/install.sh | sh -
+		source ~/.bashrc
 		# remove old node modules
-		sudo rm -rf $SRC_DIR/node_modules
+		rm -rf $SRC_DIR/node_modules
 		pnpm_install
 	fi
 }
@@ -56,25 +56,30 @@ verify_users()
 
 ensure_sudo_command_whitelisting()
 {
+	sudo="sudo"
+	if [ "$1" = "root" ]
+	then
+		sudo=""
+	fi
     report_status "Updating whitelisted commands"
 	# Whitelist RatOS configurator git hook scripts
 	if [[ -e /etc/sudoers.d/030-ratos-configurator-githooks ]]
 	then
-		sudo rm /etc/sudoers.d/030-ratos-configurator-githooks
+		$sudo rm /etc/sudoers.d/030-ratos-configurator-githooks
 	fi
 	touch /tmp/030-ratos-configurator-githooks
 	cat << __EOF > /tmp/030-ratos-configurator-githooks
 pi  ALL=(ALL) NOPASSWD: $SCRIPT_DIR/update.sh
 __EOF
 
-	sudo chown root:root /tmp/030-ratos-configurator-githooks
-	sudo chmod 440 /tmp/030-ratos-configurator-githooks
-	sudo cp --preserve=mode /tmp/030-ratos-configurator-githooks /etc/sudoers.d/030-ratos-configurator-githooks
+	$sudo chown root:root /tmp/030-ratos-configurator-githooks
+	$sudo chmod 440 /tmp/030-ratos-configurator-githooks
+	$sudo cp --preserve=mode /tmp/030-ratos-configurator-githooks /etc/sudoers.d/030-ratos-configurator-githooks
 
 	# Whitelist configurator scripts
 	if [[ -e /etc/sudoers.d/030-ratos-configurator-scripts ]]
 	then
-		sudo rm /etc/sudoers.d/030-ratos-configurator-scripts
+		$sudo rm /etc/sudoers.d/030-ratos-configurator-scripts
 	fi
 	touch /tmp/030-ratos-configurator-scripts
 	cat << __EOF > /tmp/031-ratos-configurator-scripts
@@ -84,14 +89,14 @@ pi  ALL=(ALL) NOPASSWD: $SCRIPT_DIR/dfu-flash.sh
 pi  ALL=(ALL) NOPASSWD: $SCRIPT_DIR/board-script.sh
 __EOF
 
-	sudo chown root:root /tmp/031-ratos-configurator-scripts
-	sudo chmod 440 /tmp/031-ratos-configurator-scripts
-	sudo cp --preserve=mode /tmp/031-ratos-configurator-scripts /etc/sudoers.d/031-ratos-configurator-scripts
+	$sudo chown root:root /tmp/031-ratos-configurator-scripts
+	$sudo chmod 440 /tmp/031-ratos-configurator-scripts
+	$sudo cp --preserve=mode /tmp/031-ratos-configurator-scripts /etc/sudoers.d/031-ratos-configurator-scripts
 
 	# Whitelist configurator commands
 	if [[ -e /etc/sudoers.d/031-ratos-configurator-wifi ]]
 	then
-		sudo rm /etc/sudoers.d/031-ratos-configurator-wifi
+		$sudo rm /etc/sudoers.d/031-ratos-configurator-wifi
 	fi
 	touch /tmp/031-ratos-configurator-wifi
 	cat << __EOF > /tmp/031-ratos-configurator-wifi
@@ -99,8 +104,8 @@ pi  ALL=(ALL) NOPASSWD: /usr/sbin/iw
 pi  ALL=(ALL) NOPASSWD: /usr/sbin/wpa_cli
 __EOF
 
-	sudo chown root:root /tmp/031-ratos-configurator-wifi
-	sudo chmod 440 /tmp/031-ratos-configurator-wifi
-	sudo cp --preserve=mode /tmp/031-ratos-configurator-wifi /etc/sudoers.d/031-ratos-configurator-wifi
+	$sudo chown root:root /tmp/031-ratos-configurator-wifi
+	$sudo chmod 440 /tmp/031-ratos-configurator-wifi
+	$sudo cp --preserve=mode /tmp/031-ratos-configurator-wifi /etc/sudoers.d/031-ratos-configurator-wifi
 
 }
