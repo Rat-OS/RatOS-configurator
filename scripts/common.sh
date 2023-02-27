f6775d081148a9d52a8af9d0f0c6828cfe4a8ada
miklschmidt <mikkel.schmidt@gmail.com>
2023-02-27 23:05:03 +0000

    Build: (4816351) Scripts: fix moonraker service permission

--- scripts/common.sh
+++ scripts/common.sh
@@ -29,6 +29,14 @@ ensure_pnpm_installation() {
 	fi
 }
 
+ensure_service_permission()
+{
+	report_status "Updating service permissions"
+	if ! cat /home/pi/printer_data/moonraker.asvc | grep "ratos-configurator" &>/dev/null; then
+		printf '\nratos-configurator' >> /home/pi/printer_data/moonraker.asvc
+		report_status "Configurator added to moonraker service permissions"
+	fi
+}
 
 build() {
     pushd "$SCRIPT_DIR/.." || exit 1
