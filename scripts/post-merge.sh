0632ebf2cba5273992bcca7e5456caf0c03c88d2
miklschmidt <miklschmidt@users.noreply.github.com>
2024-01-05 05:45:09 +0000

    Build: (d24c77b) Actually fix script_dir statements

--- scripts/post-merge.sh
+++ scripts/post-merge.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$(realpath "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )")
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
 # Script dir is scripts
 sudo "$SCRIPT_DIR"/update.sh
