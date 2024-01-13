437da0769d3e944140a2c4509e644359cf39bd5f
Mikkel Schmidt <msc@orbit.online>
2024-01-13 06:32:32 +0100

    Fix klipper restart method

--- src/server/helpers/klipper.ts
+++ src/server/helpers/klipper.ts
@@ -2,9 +2,9 @@ import { MoonrakerPrinterState, parseMoonrakerHTTPResponse } from '../../zods/mo
 
 export const klipperRestart = async (force = false) => {
 	const printerState = (await parseMoonrakerHTTPResponse(
-		await fetch('http://localhost:7125/printer/objects/query?query=printer'),
+		await fetch('http://localhost:7125/printer/objects/query?print_stats'),
 		MoonrakerPrinterState,
-	)).result.status.print_state.state;
+	)).result.status.print_stats.state;
 	if (force || ['error', 'complete', 'canceled', 'standby'].includes(printerState)) {
 		await fetch('http://localhost:7125/printer/restart', { method: 'POST' });
 		return true;
