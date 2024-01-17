f28cfb453e394261ee08d3126dea1486cf51ad7b
Mikkel Schmidt <msc@orbit.online>
2024-01-17 03:58:07 +0100

    More resilient flash-path script

--- src/scripts/flash-path.sh
+++ src/scripts/flash-path.sh
@@ -11,17 +11,38 @@ if [ "$MCU" == "" ]; then
 fi
 pushd /home/pi/klipper || exit
 service klipper stop
-echo "Flashing $MCU at $FLASH_PATH"
-make flash FLASH_DEVICE="$FLASH_PATH"
-chown pi:pi -R /home/pi/klipper
-sleep 5
-if [ -h "$MCU" ]; then
-    echo "Flashing Successful!"
+dfuDevicesPreFlash=$(lsusb | grep -c "0483:df11")
+
+if [ -h "$FLASH_PATH" ]; then
+    echo "Flashing $MCU at $FLASH_PATH"
+    make flash FLASH_DEVICE="$FLASH_PATH"
 else
-    echo "Flashing failed :("
-    service klipper start
-    popd || exit
-    exit 1
+  echo "$MCU does not seems to be connected, at least it was not found at $FLASH_PATH"
+  exit 1;
 fi
+sleep 5
+
+retVal=1
+
+if [ -h "$MCU" ]; then
+	retVal=0
+else
+	dfuDevicesPostFlash=$(lsusb | grep -c "0483:df11")
+	if [ "$dfuDevicesPreFlash" -eq 0 ] && [ "$dfuDevicesPostFlash" -eq 1 ]; then
+		echo "Seems like flashing failed, but the device is still in DFU mode. Attempting to recover."
+		make flash FLASH_DEVICE=0483:df11
+		sleep 5
+		if [ -h "$MCU" ]; then
+			retVal=0
+		fi
+	fi
+fi
+if [ $retVal -eq 0 ]; then
+	echo "Flashing successful."
+else
+	echo "Flashing failed."
+fi
+chown pi:pi -R /home/pi/klipper
 service klipper start
-popd || exit
+popd || exit $retVal
+exit $retVal
