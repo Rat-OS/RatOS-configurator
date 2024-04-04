c8a6da6aa4d95afda3a00fc7efa59654f0c6686b
miklschmidt <miklschmidt@users.noreply.github.com>
2024-04-04 12:14:06 +0000

    Build: (0c3bcc6) Move ignore of scichart to root so it's included in builds

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -31,8 +31,8 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/debug-zip`
     | `/api/dfu-image`
-    | `/api/printer-image`
     | `/api/download-firmware`
+    | `/api/printer-image`
     | `/`
     | `/analysis/macros/new`
     | `/analysis`
@@ -43,9 +43,9 @@ declare namespace __next_route_internal_types__ {
     | `/analysis/macros`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
