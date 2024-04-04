a26462eb78571e673b1932de61f79588ee9beb1f
Mikkel Schmidt <msc@orbit.online>
2024-04-04 22:50:37 +0200

    Fix analysis macro create

--- src/app/analysis/macros/components/macro-form.tsx
+++ src/app/analysis/macros/components/macro-form.tsx
@@ -132,200 +132,202 @@ export const MacroForm: React.FC<MacroFormProps> = ({ form, submit }) => {
 				<ResizablePanelGroup direction="horizontal" className="flex max-h-full flex-1">
 					<ResizablePanel defaultSize={35} className="flex" minSize={20}>
 						<ScrollContainer className="flex-1 overflow-y-scroll p-4">
-							<Reorder.Group
-								axis="y"
-								as="div"
-								className="relative flex flex-col items-start gap-4"
-								values={form.getValues(`sequences`)}
-								onReorder={(seqs) => form.setValue('sequences', seqs)}
-							>
-								<AnimatePresence>
-									{sequences.fields.map((sequence, index) => {
-										return (
-											<Reorder.Item
-												as="div"
-												className="grid w-full items-start gap-6"
-												key={form.getValues(`sequences.${index}.id`) ?? sequence.id}
-												exit={{
-													opacity: 0,
-													scale: 1,
-													y: -10,
-													transition: { duration: 0.2 },
-												}}
-												initial={{ opacity: 0, scale: 1, y: -10 }}
-												animate={{
-													opacity: 1,
-													scale: 1,
-													y: 0,
-													transition: { delay: 0.2, staggerChildren: 0.2 },
-												}}
-												value={form.getValues(`sequences.${index}`)}
-											>
-												<Fieldset className="grid gap-6 p-4">
-													<legend className="-ml-1 px-1 text-sm font-medium">
-														{form.watch(`sequences.${index}.name`) ?? `Sequence ${index + 1}`}
-													</legend>
-													<div className="absolute -right-2 -top-5">
-														<Button variant="danger" size="icon-xs" onClick={() => sequences.remove(index)}>
-															<X className="size-3" />
-														</Button>
-													</div>
-													<div className="grid gap-3">
-														<FormField
-															control={form.control}
-															name={`sequences.${index}.name`}
-															render={({ field }) => (
-																<FormItem>
-																	<FormLabel className="text-base">Sequence name</FormLabel>
-																	<FormControl>
-																		<Input type="text" {...field} />
-																	</FormControl>
-																	<FormMessage />
-																</FormItem>
-															)}
-														/>
-													</div>
-													<div className="grid gap-3">
-														<FormField
-															control={form.control}
-															name={`sequences.${index}.gcode`}
-															render={({ field }) => (
-																<FormItem>
-																	<FormLabel className="text-base">G-code</FormLabel>
-																	<FormControl>
-																		<Textarea
-																			id="temperature"
-																			placeholder={'; Example \nOSCILLATE FREQ=60 TIME=1'}
-																			{...field}
-																		/>
-																	</FormControl>
-																	<FormMessage />
-																</FormItem>
-															)}
-														/>
-													</div>
-													<div className="grid gap-3 rounded-lg border p-4 @container">
-														<FormField
-															control={form.control}
-															name={`sequences.${index}.recording.capturePSD`}
-															render={({ field }) => (
-																<FormItem className="flex flex-row items-center justify-between">
-																	<div className="space-y-0.5">
-																		<FormLabel className="text-base">Record resonances</FormLabel>
-																		<FormDescription className="font-medium">
-																			Enable the calculation and persistence of power spectral density.
-																		</FormDescription>
-																	</div>
-																	<FormControl>
-																		<Switch checked={field.value} onCheckedChange={field.onChange} />
-																	</FormControl>
-																	<FormMessage />
-																</FormItem>
-															)}
-														/>
-														<AnimatedContainer>
-															<AnimatePresence>
-																{form.watch(`sequences.${index}.recording.capturePSD`) && (
-																	<motion.div
-																		exit={{
-																			opacity: 0,
-																			scale: 1,
-																			y: -10,
-																			transition: { duration: 0.2 },
-																		}}
-																		initial={{ opacity: 0, scale: 1, y: -10 }}
-																		animate={{
-																			opacity: 1,
-																			scale: 1,
-																			y: 0,
-																			transition: { delay: 0.2, staggerChildren: 0.2 },
-																		}}
-																		className="grid gap-3 @xs:grid-cols-2"
-																	>
-																		<FormField
-																			control={form.control}
-																			name={`sequences.${index}.recording.accelerometer`}
-																			render={({ field }) => (
-																				<FormItem>
-																					<FormLabel>Accelerometer</FormLabel>
-																					<FormControl>
-																						<Select onValueChange={field.onChange} value={field.value}>
-																							<SelectTrigger
-																								id="model"
-																								className="items-start [&_[data-description]]:hidden"
-																							>
-																								<SelectValue placeholder="Select an accelerometer..." />
-																							</SelectTrigger>
-																							<SelectContent>
-																								{labels.map(({ label, value, icon: Icon, description }) => (
-																									<SelectItem value={value} key={label}>
-																										<div className="flex items-start gap-1 text-muted-foreground">
-																											<Icon className="size-5 shrink-0" />
-																											<div className="grid gap-0.5">
-																												<p>
-																													<span className="font-medium text-foreground">{label}</span>
-																												</p>
-																												<p className="text-sm" data-description>
-																													{description}
-																												</p>
-																											</div>
-																										</div>
-																									</SelectItem>
-																								))}
-																							</SelectContent>
-																						</Select>
-																					</FormControl>
-																					<FormMessage />
-																				</FormItem>
-																			)}
-																		/>
-																		<FormField
-																			control={form.control}
-																			name={`sequences.${index}.recording.color`}
-																			render={({ field }) => (
-																				<FormItem>
-																					<FormLabel>Sequence Color</FormLabel>
-																					<FormControl>
-																						<Select onValueChange={field.onChange} value={field.value}>
-																							<SelectTrigger id="sequence-color" className="items-start">
-																								<SelectValue placeholder="Select a color..." />
-																							</SelectTrigger>
-																							<SelectContent>
-																								{colorOptions.map(({ label, color, value }) => (
-																									<SelectItem value={value} key={value}>
-																										<div className="flex items-start gap-2 text-muted-foreground">
-																											<div
-																												className="size-5 rounded"
-																												style={{ backgroundColor: color }}
-																											/>
-																											<div className="grid gap-0.5">
-																												<p>
-																													<span className="font-medium capitalize text-foreground">
-																														{label}
-																													</span>
-																												</p>
-																											</div>
-																										</div>
-																									</SelectItem>
-																								))}
-																							</SelectContent>
-																						</Select>
-																					</FormControl>
-																					<FormMessage />
-																				</FormItem>
-																			)}
-																		/>
-																	</motion.div>
+							{(form.getValues(`sequences`)?.length ?? 0) > 0 && (
+								<Reorder.Group
+									axis="y"
+									as="div"
+									className="relative flex flex-col items-start gap-4"
+									values={form.getValues(`sequences`)}
+									onReorder={(seqs) => form.setValue('sequences', seqs)}
+								>
+									<AnimatePresence>
+										{sequences.fields.map((sequence, index) => {
+											return (
+												<Reorder.Item
+													as="div"
+													className="grid w-full items-start gap-6"
+													key={form.getValues(`sequences.${index}.id`) ?? sequence.id}
+													exit={{
+														opacity: 0,
+														scale: 1,
+														y: -10,
+														transition: { duration: 0.2 },
+													}}
+													initial={{ opacity: 0, scale: 1, y: -10 }}
+													animate={{
+														opacity: 1,
+														scale: 1,
+														y: 0,
+														transition: { delay: 0.2, staggerChildren: 0.2 },
+													}}
+													value={form.getValues(`sequences.${index}`)}
+												>
+													<Fieldset className="grid gap-6 p-4">
+														<legend className="-ml-1 px-1 text-sm font-medium">
+															{form.watch(`sequences.${index}.name`) ?? `Sequence ${index + 1}`}
+														</legend>
+														<div className="absolute -right-2 -top-5">
+															<Button variant="danger" size="icon-xs" onClick={() => sequences.remove(index)}>
+																<X className="size-3" />
+															</Button>
+														</div>
+														<div className="grid gap-3">
+															<FormField
+																control={form.control}
+																name={`sequences.${index}.name`}
+																render={({ field }) => (
+																	<FormItem>
+																		<FormLabel className="text-base">Sequence name</FormLabel>
+																		<FormControl>
+																			<Input type="text" {...field} />
+																		</FormControl>
+																		<FormMessage />
+																	</FormItem>
 																)}
-															</AnimatePresence>
-														</AnimatedContainer>
-													</div>
-												</Fieldset>
-											</Reorder.Item>
-										);
-									})}
-								</AnimatePresence>
-							</Reorder.Group>
+															/>
+														</div>
+														<div className="grid gap-3">
+															<FormField
+																control={form.control}
+																name={`sequences.${index}.gcode`}
+																render={({ field }) => (
+																	<FormItem>
+																		<FormLabel className="text-base">G-code</FormLabel>
+																		<FormControl>
+																			<Textarea
+																				id="temperature"
+																				placeholder={'; Example \nOSCILLATE FREQ=60 TIME=1'}
+																				{...field}
+																			/>
+																		</FormControl>
+																		<FormMessage />
+																	</FormItem>
+																)}
+															/>
+														</div>
+														<div className="grid gap-3 rounded-lg border p-4 @container">
+															<FormField
+																control={form.control}
+																name={`sequences.${index}.recording.capturePSD`}
+																render={({ field }) => (
+																	<FormItem className="flex flex-row items-center justify-between">
+																		<div className="space-y-0.5">
+																			<FormLabel className="text-base">Record resonances</FormLabel>
+																			<FormDescription className="font-medium">
+																				Enable the calculation and persistence of power spectral density.
+																			</FormDescription>
+																		</div>
+																		<FormControl>
+																			<Switch checked={field.value} onCheckedChange={field.onChange} />
+																		</FormControl>
+																		<FormMessage />
+																	</FormItem>
+																)}
+															/>
+															<AnimatedContainer>
+																<AnimatePresence>
+																	{form.watch(`sequences.${index}.recording.capturePSD`) && (
+																		<motion.div
+																			exit={{
+																				opacity: 0,
+																				scale: 1,
+																				y: -10,
+																				transition: { duration: 0.2 },
+																			}}
+																			initial={{ opacity: 0, scale: 1, y: -10 }}
+																			animate={{
+																				opacity: 1,
+																				scale: 1,
+																				y: 0,
+																				transition: { delay: 0.2, staggerChildren: 0.2 },
+																			}}
+																			className="grid gap-3 @xs:grid-cols-2"
+																		>
+																			<FormField
+																				control={form.control}
+																				name={`sequences.${index}.recording.accelerometer`}
+																				render={({ field }) => (
+																					<FormItem>
+																						<FormLabel>Accelerometer</FormLabel>
+																						<FormControl>
+																							<Select onValueChange={field.onChange} value={field.value}>
+																								<SelectTrigger
+																									id="model"
+																									className="items-start [&_[data-description]]:hidden"
+																								>
+																									<SelectValue placeholder="Select an accelerometer..." />
+																								</SelectTrigger>
+																								<SelectContent>
+																									{labels.map(({ label, value, icon: Icon, description }) => (
+																										<SelectItem value={value} key={label}>
+																											<div className="flex items-start gap-1 text-muted-foreground">
+																												<Icon className="size-5 shrink-0" />
+																												<div className="grid gap-0.5">
+																													<p>
+																														<span className="font-medium text-foreground">{label}</span>
+																													</p>
+																													<p className="text-sm" data-description>
+																														{description}
+																													</p>
+																												</div>
+																											</div>
+																										</SelectItem>
+																									))}
+																								</SelectContent>
+																							</Select>
+																						</FormControl>
+																						<FormMessage />
+																					</FormItem>
+																				)}
+																			/>
+																			<FormField
+																				control={form.control}
+																				name={`sequences.${index}.recording.color`}
+																				render={({ field }) => (
+																					<FormItem>
+																						<FormLabel>Sequence Color</FormLabel>
+																						<FormControl>
+																							<Select onValueChange={field.onChange} value={field.value}>
+																								<SelectTrigger id="sequence-color" className="items-start">
+																									<SelectValue placeholder="Select a color..." />
+																								</SelectTrigger>
+																								<SelectContent>
+																									{colorOptions.map(({ label, color, value }) => (
+																										<SelectItem value={value} key={value}>
+																											<div className="flex items-start gap-2 text-muted-foreground">
+																												<div
+																													className="size-5 rounded"
+																													style={{ backgroundColor: color }}
+																												/>
+																												<div className="grid gap-0.5">
+																													<p>
+																														<span className="font-medium capitalize text-foreground">
+																															{label}
+																														</span>
+																													</p>
+																												</div>
+																											</div>
+																										</SelectItem>
+																									))}
+																								</SelectContent>
+																							</Select>
+																						</FormControl>
+																						<FormMessage />
+																					</FormItem>
+																				)}
+																			/>
+																		</motion.div>
+																	)}
+																</AnimatePresence>
+															</AnimatedContainer>
+														</div>
+													</Fieldset>
+												</Reorder.Item>
+											);
+										})}
+									</AnimatePresence>
+								</Reorder.Group>
+							)}
 							<Button
 								variant="outline"
 								className="mt-4 w-full justify-center text-center"
