f4cd2131b15d62798b4631fd4cd7bb83fd11ff7f
miklschmidt <miklschmidt@users.noreply.github.com>
2024-04-16 10:21:07 +0000

    Build: (8c86fb0) Improve printer state detection

--- server/helpers/klipper.ts
+++ server/helpers/klipper.ts
@@ -1,14 +1,25 @@
-import { MoonrakerPrinterState, parseMoonrakerHTTPResponse } from '@/zods/moonraker';
+import { MoonrakerPrinterState, MoonrakerPrinterStateErrorEnum, parseMoonrakerHTTPResponse } from '@/zods/moonraker';
 
 export const queryPrinterState = async (): Promise<
 	Zod.output<typeof MoonrakerPrinterState>['status']['print_stats']['state']
 > => {
-	return (
-		await parseMoonrakerHTTPResponse(
-			await fetch('http://localhost:7125/printer/objects/query?print_stats'),
-			MoonrakerPrinterState,
-		)
-	).result.status.print_stats.state;
+	try {
+		const moonrakerRes = await fetch('http://localhost:7125/printer/objects/query?print_stats');
+		if (moonrakerRes)
+			return (await parseMoonrakerHTTPResponse(moonrakerRes, MoonrakerPrinterState)).result.status.print_stats.state;
+	} catch (e) {
+		if (
+			e instanceof Error &&
+			e.cause ===
+				(MoonrakerPrinterStateErrorEnum.MOONRAKER_OFFLINE ||
+					e.cause === MoonrakerPrinterStateErrorEnum.MOONRAKER_INTERNAL_ERROR)
+		) {
+			return 'error';
+		} else {
+			throw e;
+		}
+	}
+	return 'error';
 };
 
 export const klipperRestart = async (force = false) => {
--- zods/moonraker.tsx
+++ zods/moonraker.tsx
@@ -23,10 +23,27 @@ export const MoonrakerHTTPResponse = z.object({
 	result: MoonrakerBaseResult.passthrough(),
 });
 
+export enum MoonrakerPrinterStateErrorEnum {
+	'MOONRAKER_OFFLINE',
+	'MOONRAKER_INTERNAL_ERROR',
+}
+
 export const parseMoonrakerHTTPResponse = async <T extends z.ZodType<z.output<typeof MoonrakerBaseResult>>>(
 	response: Response,
 	responseZod: T,
 ): Promise<z.output<typeof MoonrakerHTTPResponse> & { result: z.output<T> }> => {
+	if (response.status !== 200) {
+		const error = await response.json();
+		getLogger().error(error, 'Moonraker internal error');
+		if (error != null && typeof error === 'object' && 'error' in error && typeof error.error === 'string') {
+			const toThrow = new Error(error.error);
+			toThrow.cause = MoonrakerPrinterStateErrorEnum.MOONRAKER_INTERNAL_ERROR;
+			throw toThrow;
+		}
+		const toThrow = new Error('Error fetching moonraker response');
+		toThrow.cause = MoonrakerPrinterStateErrorEnum.MOONRAKER_OFFLINE;
+		throw new Error('Error fetching moonraker response');
+	}
 	const jsonResult = await response.json();
 	try {
 		const res = MoonrakerHTTPResponse.parse(jsonResult);
