5f1cc393d1f462177cf59545ba0a39276c4b2011
miklschmidt <miklschmidt@users.noreply.github.com>
2024-05-19 01:46:03 +0000

    Build: (a0f86c7) Better spinner

--- app/calibration/page.tsx
+++ app/calibration/page.tsx
@@ -4,12 +4,22 @@ import { FullLoadScreen } from '@/components/common/full-load-screen';
 import { NoSSR } from '@/components/common/no-ssr';
 import React from 'react';
 import { VAOC } from '@/app/calibration/vaoc';
+import { Spinner } from '@/components/common/spinner';
 
 export default function Page() {
+	const fallback = (
+		<div className="absolute inset-0 flex h-[calc(100vh_-_64px)] items-center justify-center">
+			<Spinner
+				className="h-[40svh] w-[40svh] animate-spin transition-all dark:text-lime-400"
+				noMargin={true}
+				strokeWidth={1}
+			/>
+		</div>
+	);
 	return (
-		<div className="h-full @container">
-			<React.Suspense fallback={<FullLoadScreen />}>
-				<NoSSR>
+		<div className="flex h-full w-full items-center @container">
+			<React.Suspense fallback={fallback}>
+				<NoSSR fallback={fallback}>
 					<VAOC />
 				</NoSSR>
 			</React.Suspense>
--- components/common/spinner.tsx
+++ components/common/spinner.tsx
@@ -1,10 +1,10 @@
 import { twMerge } from 'tailwind-merge';
 
-export const Spinner: React.FC<{ className?: string; noMargin?: boolean }> = (props) => {
+export const Spinner: React.FC<{ className?: string; noMargin?: boolean; strokeWidth?: number }> = (props) => {
 	return (
 		<svg
 			className={twMerge(
-				'h-5 w-5 animate-spin text-black dark:text-white',
+				'h-5 w-5 animate-spin text-black/50 dark:text-white/50',
 				!props.noMargin ? '-ml-1 mr-3' : '',
 				props.className,
 			)}
@@ -12,12 +12,24 @@ export const Spinner: React.FC<{ className?: string; noMargin?: boolean }> = (pr
 			fill="none"
 			viewBox="0 0 24 24"
 		>
-			<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-			<path
-				className="opacity-75"
-				fill="currentColor"
-				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-			></path>
+			<circle
+				className="opacity-20"
+				cx="12"
+				cy="12"
+				r="10"
+				stroke="currentColor"
+				strokeWidth={props.strokeWidth ?? 4}
+			></circle>
+			<circle
+				className="opacity-100"
+				cx="12"
+				cy="12"
+				r="10"
+				strokeDasharray={(Math.PI * 10) / 3}
+				strokeLinecap="round"
+				stroke="currentColor"
+				strokeWidth={props.strokeWidth ?? 4}
+			></circle>
 		</svg>
 	);
 };
