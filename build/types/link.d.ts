5ccf5433f79ffb84b8c3379778ea09b04f4ae28d
miklschmidt <miklschmidt@users.noreply.github.com>
2024-05-24 11:40:14 +0000

    Build: (41551e9) Fix axis_map => axes_map

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -36,10 +36,10 @@ declare namespace __next_route_internal_types__ {
     | `/`
     | `/calibration`
     | `/motion`
-    | `/wizard`
     | `/toolhead`
-    | `/analysis/macros/new`
+    | `/wizard`
     | `/analysis/macros`
+    | `/analysis/macros/new`
     | `/analysis`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
