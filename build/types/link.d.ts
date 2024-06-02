220d8094f4dc1093f28e43b68369ab910ca31b5c
miklschmidt <miklschmidt@users.noreply.github.com>
2024-06-02 03:24:37 +0000

    Build: (3d268a5) UI: minor issue fixes and beacon flash via help menu

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -35,17 +35,17 @@ declare namespace __next_route_internal_types__ {
     | `/api/printer-image`
     | `/`
     | `/calibration`
-    | `/toolhead`
     | `/motion`
+    | `/toolhead`
     | `/wizard`
-    | `/analysis/macros/new`
     | `/analysis/macros`
+    | `/analysis/macros/new`
     | `/analysis`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
 
   type RouteImpl<T> = 
