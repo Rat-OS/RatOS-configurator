ca0bee6a7418fa7a1ca3a66a4d09494dfba57ce4
miklschmidt <miklschmidt@users.noreply.github.com>
2024-06-07 15:07:15 +0000

    Build: (bd7ab74) Add cfg and json from ratos data dir to debug zip

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -30,8 +30,8 @@ declare namespace __next_route_internal_types__ {
 
   type StaticRoutes = 
     | `/api/dfu-image`
-    | `/api/download-firmware`
     | `/api/mcu-image`
+    | `/api/download-firmware`
     | `/api/printer-image`
     | `/`
     | `/calibration`
