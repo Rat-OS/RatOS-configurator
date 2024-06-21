3386e61c0b0fc1829a0e33a136d4f289f1e56d97
miklschmidt <miklschmidt@users.noreply.github.com>
2024-06-21 00:14:38 +0000

    Build: (d6461a6) Wizard: cleanup hidden ssid checks

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -34,19 +34,19 @@ declare namespace __next_route_internal_types__ {
     | `/api/mcu-image`
     | `/api/printer-image`
     | `/`
-    | `/motion`
     | `/calibration`
+    | `/motion`
     | `/toolhead`
     | `/wizard`
-    | `/analysis/macros/new`
     | `/analysis`
+    | `/analysis/macros/new`
     | `/analysis/macros`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
