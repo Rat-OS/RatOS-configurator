4532ffcee5ac972163f111b9d70a2867bdc93b4d
miklschmidt <miklschmidt@users.noreply.github.com>
2024-07-04 15:58:48 +0000

    Build: (5a0ad9e) Templates: home y first on v-chonk

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -39,8 +39,8 @@ declare namespace __next_route_internal_types__ {
     | `/toolhead`
     | `/wizard`
     | `/analysis`
-    | `/analysis/macros/new`
     | `/analysis/macros`
+    | `/analysis/macros/new`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
