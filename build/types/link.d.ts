f72f1cb097c5c9f4f6d9a3ba53db2ad4a180547e
miklschmidt <miklschmidt@users.noreply.github.com>
2024-07-17 00:26:35 +0000

    Build: (0ed0c12) ConfigGen: support invertedPins and customSections in board definitions

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -31,20 +31,20 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/dfu-image`
     | `/api/download-firmware`
-    | `/api/mcu-image`
     | `/api/printer-image`
+    | `/api/mcu-image`
     | `/`
     | `/motion`
     | `/calibration`
     | `/toolhead`
     | `/wizard`
+    | `/analysis/macros/new`
     | `/analysis/macros`
     | `/analysis`
-    | `/analysis/macros/new`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}`
+    | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
 
