bf89b5e58a39d40c9fc9fd60d0f75a45c4d023f0
miklschmidt <miklschmidt@users.noreply.github.com>
2024-07-17 01:23:50 +0000

    Build: (fb58977) Workflows: checkout the right branch in deployment scripts

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -31,11 +31,11 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/dfu-image`
     | `/api/download-firmware`
-    | `/api/printer-image`
     | `/api/mcu-image`
+    | `/api/printer-image`
     | `/`
-    | `/motion`
     | `/calibration`
+    | `/motion`
     | `/toolhead`
     | `/wizard`
     | `/analysis/macros/new`
@@ -43,8 +43,8 @@ declare namespace __next_route_internal_types__ {
     | `/analysis`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
+    | `/analysis/macros/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
 
