ec949edf7b66a0e04d8bcc816f788bb830de5643
miklschmidt <miklschmidt@users.noreply.github.com>
2024-08-20 14:33:41 +0000

    Build: (a4ddd9f) ConfigGen: endsWith('_pin') is safer than endsWith('pin')

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -31,22 +31,22 @@ declare namespace __next_route_internal_types__ {
   type StaticRoutes = 
     | `/api/dfu-image`
     | `/api/download-firmware`
-    | `/api/mcu-image`
     | `/api/printer-image`
+    | `/api/mcu-image`
     | `/`
     | `/calibration`
     | `/toolhead`
     | `/motion`
     | `/wizard`
-    | `/analysis/macros/new`
-    | `/analysis/macros`
     | `/analysis`
+    | `/analysis/macros`
+    | `/analysis/macros/new`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
