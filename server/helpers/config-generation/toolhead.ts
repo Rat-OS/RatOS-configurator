93b296893ec6f1aa7419fa165c45c105406c3cf1
miklschmidt <miklschmidt@users.noreply.github.com>
2024-08-20 14:33:37 +0000

    Build: (a4ddd9f) ConfigGen: endsWith('_pin') is safer than endsWith('pin')

--- server/helpers/config-generation/toolhead.ts
+++ server/helpers/config-generation/toolhead.ts
@@ -291,7 +291,7 @@ export class ToolheadGenerator<IsToolboard extends boolean> extends ToolheadHelp
 					}
 					if (
 						typeof value == 'string' &&
-						(parameter.endsWith('pin') || Object.values(this.toolboardPins).includes(value))
+						(parameter.endsWith('_pin') || Object.values(this.toolboardPins).includes(value))
 					) {
 						result.push(`${parameter}: ${this.isToolboardPinInverted(value) ? '!' : ''}${this.getPinPrefix()}${value}`);
 					} else {
