04e7c6f3ca81a691b9babf23a9f118c62014afe9
Mikkel Schmidt <msc@orbit.online>
2024-08-23 02:50:25 +0200

    CLI: Add possibility to switch frontend update channel

---
+++ src/server/helpers/config-parsing.ts
@@ -0,0 +1,34 @@
+export type SectionProperties = { [property: string]: string };
+export const findSection = (section: string, content: string) => {
+	const regex = new RegExp(`(?<header>\\[${section}\\])\\n(?<body>(?:^(?:\\S| )+$(\\n?))+)`, 'gm');
+	let match = null;
+	const matches: {
+		header: string;
+		body: string;
+		properties: SectionProperties;
+		start: number;
+		end: number;
+		content: string;
+	}[] = [];
+	while ((match = regex.exec(content)) != null) {
+		const header = match.groups?.header;
+		const body = match.groups?.body;
+		if (header == null || body == null) {
+			continue;
+		}
+		const properties = body.split('\n').reduce((acc, line) => {
+			if (line.trim() === '') {
+				return acc;
+			}
+			const [property, ...valueFragments] = line.split(':');
+			const value = valueFragments.join(':').trim();
+			acc[property] = value;
+			return acc;
+		}, {} as SectionProperties);
+		matches.push({ header, body, properties, start: match.index, end: regex.lastIndex, content: match[0] });
+	}
+	if (!matches) {
+		return null;
+	}
+	return matches;
+};
