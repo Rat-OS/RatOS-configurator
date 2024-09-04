c237f483cd3a3f0396c84835560a7629df1453d0
miklschmidt <miklschmidt@users.noreply.github.com>
2024-09-04 22:32:57 +0000

    Build: (bfeb8dd) MCU: fix board version checking (#19)

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -39,14 +39,14 @@ declare namespace __next_route_internal_types__ {
     | `/toolhead`
     | `/wizard`
     | `/analysis/macros/new`
-    | `/analysis/macros`
     | `/analysis`
+    | `/analysis/macros`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/edit`
     | `/analysis/macros/${SafeSlug<T>}`
-    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
     | `/analysis/macros/${SafeSlug<T>}/recordings`
+    | `/analysis/macros/${SafeSlug<T>}/recordings/${SafeSlug<T>}`
 
   type RouteImpl<T> = 
     | StaticRoutes
